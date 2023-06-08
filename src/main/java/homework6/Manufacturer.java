7821f5961e2f9f659f8145424c4904a70aaf7176
Petrachkova <disprove69@gmail.com>
2023-06-08 23:21:35 +0300

    go

---
+++ src/main/java/homework6/Manufacturer.java
@@ -0,0 +1,20 @@
+package homework6;
+
+public enum Manufacturer {
+    ACER,
+    APPLE,
+    ASUS,
+    HONOR,
+    HUAWEI,
+    LENOVO,
+    SAMSUNG,
+    MSI,
+    HP;
+
+/**    public static Manufacturer generateRandomManufacture() {
+        Manufacturer[] values = Manufacturer.values();
+        int length = values.length;
+        int randIndex = new Random().nextInt(length);
+        return values[randIndex];
+    }**/
+}
